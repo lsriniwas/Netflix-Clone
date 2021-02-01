eac0fab96d6c34fd69df719fe055d453d0621c5e
Mahen Paraameshwar <mahe365param@gmail.com>
2021-02-01 20:28:40 +0530

    Login and Register feature implemented

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 
+const registerRoute = require("./Routes/register");
+const loginRoute = require("./Routes/login");
+
 const connectDB = require("./Config/db");
 
 const dotenv = require("dotenv");
@@ -13,8 +16,10 @@ dotenv.config();
 connectDB();
 
 app.use(express.json());
-
 app.use(cors());
+
+app.use("/api", registerRoute);
+app.use("/api", loginRoute);
 
 app.listen(port, () => {
   console.log("Listeing at port", port);
